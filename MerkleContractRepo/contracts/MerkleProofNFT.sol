520c08a130355d4101450457ade4b5df254a1fbe
0xKartik-dev <karthikganiga007@gmail.com>
2022-12-27 03:35:36 +0530

    solidity code not refactored

--- MerkleContractRepo/contracts/MerkleProofNFT.sol
+++ MerkleContractRepo/contracts/MerkleProofNFT.sol
@@ -2,4 +2,28 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract MerkleProofNFT {}
+contract MerkleProofNFT is ERC721 {
+    bytes32 public mRootHash;
+
+    mapping(address => bool) public trackerList;
+
+    constructor(bytes32 _mRootHash)
+        ERC721("MerkleProof NFT Contract", "MPFNFT")
+    {
+        mRootHash = _mRootHash;
+    }
+
+    function mint(bytes32[] calldata _mProofBySender) external {
+        bytes32 leaf = keccak256(abi.encode(msg.sender));
+        require(!trackerList[msg.sender], "You have already claimed bro");
+        require(
+            MerkleProof.verify(
+                _mProofBySender,
+                mRootHash,
+                leaf,
+                "Invalid Proof bro"
+            )
+        );
+        trackerList[msg.sender] = true;
+    }
+}
